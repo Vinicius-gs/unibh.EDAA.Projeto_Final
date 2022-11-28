936f0d832288210ead9eaba2f49943d05fd33057
Vinicius-gs <souzaviniciusg@gmail.com>
2022-11-28 17:30:19 -0300

    first commit

--- src/Lista_Encadeada/Main.java
+++ src/Lista_Encadeada/Main.java
@@ -9,13 +9,11 @@ public class Main {
 		Alunos A1 = new Alunos();
 		Aluno A2 = new Aluno("Vini", 5, "CC;");
 		Aluno A3 = new Aluno("Mateus", 6, "CC;");
-		Aluno A4 = new Aluno("HB", 7, "CC");
 		
 		A1.insereInicio(A2);
 		A1.insereInicio(A3);
 		System.out.println(A1.exibeLista()); 
-		A1.insereFinal(A4);
-		System.out.println(A1.exibeLista()); 
+
 	}
 
 }
